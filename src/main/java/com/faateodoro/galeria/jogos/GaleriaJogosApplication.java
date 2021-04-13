639901977020d8968459fac7cb5b4eba745af3a7
Fabiano Teodoro <faateodoro@gmail.com>
2021-04-12 23:35:24 -0300

    Commit inicial

---
+++ src/main/java/com/faateodoro/galeria/jogos/GaleriaJogosApplication.java
@@ -0,0 +1,13 @@
+package com.faateodoro.galeria.jogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaleriaJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaleriaJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faateodoro/galeria/jogos/GaleriaJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faateodoro.galeria.jogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaleriaJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
